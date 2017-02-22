3dc207867c01797b333ef7fbd936f062faa2216b
Carter Fort <carter.fort@gmail.com>
2017-02-22 09:30:10 -0500

    Added two more rotors

---
+++ app/Enigma/rotors/RotorIV.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Enigma\Rotors;
+
+use Enigma\Rotor;
+
+class RotorIV extends Rotor
+{
+
+	protected function getSequence()
+	{
+		return [4,23,22,12,7,17,5,19,25,10,15,2,18,21,24,16,8,9,11,14,1,0,3,6,13,20];
+	}
+
+}
---
+++ app/Enigma/rotors/RotorV.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Enigma\Rotors;
+
+use Enigma\Rotor;
+
+class RotorV extends Rotor
+{
+
+	protected function getSequence()
+	{
+		return [8,21,19,18,6,13,5,0,7,3,1,4,15,10,22,20,14,12,11,24,25,17,2,9,16,23];
+	}
+
+}
